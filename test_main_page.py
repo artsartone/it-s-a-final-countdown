c20306e56b813313204821007dc7231b0434ff91
artsartone <brainxkhaled@mail.ru>
2022-10-21 13:08:21 +0300

    almost page object

--- test_main_page.py
+++ test_main_page.py
@@ -1,10 +1,14 @@
 from selenium.webdriver.common.by import By
 import time
+from .pages.main_page import MainPage
 
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
     time.sleep(5)
     login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
     login_link.click()
+
