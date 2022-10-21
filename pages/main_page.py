a5a45aaa3d9b70da02449f29d56758af50d7d7f2
artsartone <brainxkhaled@mail.ru>
2022-10-21 13:34:03 +0300

    NoSuchElementException

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,2 +1,9 @@
+from .base_page import BasePage
+from selenium.webdriver.common.by import By
+
+
+class MainPage(BasePage):
+    def should_be_login_link(self):
+        assert self.is_element_present(By.CSS_SELECTOR, "#login_link"), "Login link is not presented"
 
 
