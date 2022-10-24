5246bbe578a65016b5c0c78ab03ad3f03a43248b
artsartone <brainxkhaled@mail.ru>
2022-10-25 01:50:03 +0300

    negative checks/fake user register/mark/authorized user test/

--- test_main_page.py
+++ test_main_page.py
@@ -1,11 +1,105 @@
-import time
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
+from .pages.product_page import ProductPage
+from .pages.basket_page import BasketPage
+import time
+import pytest
 
 
+@pytest.mark.login_guest
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
+    link = "http://selenium1py.pythonanywhere.com"
     page = MainPage(browser, link)
     page.open()
     page.go_to_login_page()
-    time.sleep(5)
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
+    time.sleep(0)
 
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+
+@pytest.mark.login_guest
+def test_guest_cant_see_success_message(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_not_be_success_message()
+
+
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_to_basket()
+    page.should_not_be_success_message()
+
+
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+    link = "http://selenium1py.pythonanywhere.com"
+    page = BasketPage(browser, link)
+    page.open()
+    page.go_to_basket()
+    page.should_be_empty_basket_and_message_about_it()
+
+
+# @pytest.mark.parametrize('links',
+# ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+# "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+# "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+# "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+# "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+# "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+# "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+# pytest.param
+# ("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
+# marks=pytest.mark.xfail),
+# "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+# "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
+@pytest.mark.need_review
+def test_guest_can_add_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_to_basket()
+    # page.solve_quiz_and_get_code()
+    page.should_be_page_after_added_product()
+
+
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_to_basket()
+    page.should_be_dissapeared_success_message()
+
+
+@pytest.mark.login_user
+class TestUserAddToBasketFromProductPage:
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self, browser):
+        login_link = "http://selenium1py.pythonanywhere.com/accounts/login/"
+        self.login_link = login_link
+        self.page = LoginPage(browser, login_link)
+        self.page.open()
+        self.page.register_new_user()
+        self.page.should_be_authorized_user()
+
+    def test_user_cant_see_success_message(self, browser):
+        link_product = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209"
+        page = ProductPage(browser, link_product)
+        page.open()
+        page.should_not_be_success_message()
+
+    @pytest.mark.need_review
+    def test_user_can_add_product_to_basket(self, browser):
+        link_product = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209"
+        page = ProductPage(browser, link_product)
+        page.open()
+        page.add_to_basket()
+        page.should_be_page_after_added_product()
