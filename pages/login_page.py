ced6a10a9b8379f206d24b7a753edf288439a204
artsartone <brainxkhaled@mail.ru>
2022-10-25 02:03:34 +0300

    fill in the registration form/find register form

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,5 +1,6 @@
 from .base_page import BasePage
 from .locators import LoginPageLocators
+import time
 
 
 class LoginPage(BasePage):
@@ -9,7 +10,7 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        assert "Login" in self.browser.current_url, "Login is not presented"
+        assert "login" in self.browser.current_url, "Login is not presented"
         assert True
 
     def should_be_login_form(self):
@@ -19,3 +20,27 @@ class LoginPage(BasePage):
     def should_be_register_form(self):
         assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form is not presented"
         assert True
+
+    def register_new_user(self):
+        self.send_email()
+        self.send_pass()
+        self.send_pass2()
+        self.send_register_button()
+
+    def send_email(self):
+        email = self.browser.find_element(*LoginPageLocators.EMAIL_FORM)
+        form_email = str(time.time()) + "@fakemail.org"
+        email.send_keys(form_email)
+
+    def send_pass(self):
+        password = self.browser.find_element(*LoginPageLocators.PASSWORD_FORM)
+        password.send_keys("Don't hack me pls")
+
+    def send_pass2(self):
+        password_two = self.browser.find_element(*LoginPageLocators.PASSWORD_FORM2)
+        password_two.send_keys("Don't hack me pls")
+
+    def send_register_button(self):
+        register_button = self.browser.find_element(*LoginPageLocators.REGISTER_BUTTON)
+        register_button.click()
+
