75f358281e62325c4a552e8ac245050bb96d5a61
artsartone <brainxkhaled@mail.ru>
2022-10-25 11:14:33 +0300

    correction appearance

--- pages/base_page.py
+++ pages/base_page.py
@@ -12,27 +12,34 @@ class BasePage():
         self.url = url
         self.browser.implicitly_wait(timeout)
 
+
     def go_to_login_page(self):
         link = self.browser.find_element(*BasePageLocators.LOGIN_LINK_INVALID)
         link.click()
 
+
     def go_to_basket(self):
         link = self.browser.find_element(*BasePageLocators.BASKET_LINK_INVALID)
         link.click()
+
 
     def is_element_present(self, how, what):
         try:
             self.browser.find_element(how, what)
         except NoSuchElementException:
             return False
+
         return True
+
 
     def is_not_element_present(self, how, what, timeout=4):
         try:
             WebDriverWait(self.browser, timeout).until(EC.presence_of_element_located((how, what)))
         except TimeoutException:
             return True
+
         return False
+
 
     def is_disappeared(self, how, what, timeout=4):
         try:
@@ -40,17 +47,22 @@ class BasePage():
                 until_not(EC.presence_of_element_located((how, what)))
         except TimeoutException:
             return False
+
         return True
+
 
     def open(self):
         self.browser.get(self.url)
+
 
     def should_be_authorized_user(self):
         assert self.is_element_present(*BasePageLocators.USER_ICON), "User icon is not presented," \
                                                                      " probably unauthorised user"
 
+
     def should_be_login_link(self):
         assert self.is_element_present(*BasePageLocators.LOGIN_LINK_INVALID), "Login link is not presented"
+
 
     def solve_quiz_and_get_code(self):
         alert = self.browser.switch_to.alert
