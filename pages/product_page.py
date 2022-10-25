94d46629f5629ec4fcfeb446336a25baa22320c2
artsartone <brainxkhaled@mail.ru>
2022-10-25 11:15:29 +0300

    correction appearance

--- pages/product_page.py
+++ pages/product_page.py
@@ -1,7 +1,6 @@
 from .base_page import BasePage
 from .locators import ProductPageLocators
 
-
 class ProductPage(BasePage):
     def should_be_page_after_added_product(self):
         self.should_be_added_massage()
@@ -32,7 +31,6 @@ class ProductPage(BasePage):
     def should_not_be_success_message(self):
         assert self.is_not_element_present\
             (*ProductPageLocators.SUCCESS_MASSAGE), "Success message is presented, but should not be"
-
     def should_be_dissapeared_success_message(self):
         assert self.is_disappeared\
             (*ProductPageLocators.SUCCESS_MASSAGE), "Success message is presented, but should not be"
